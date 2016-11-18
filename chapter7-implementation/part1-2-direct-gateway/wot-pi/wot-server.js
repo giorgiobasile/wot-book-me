90a9ba2fc818f2f270d00271de84ae542d2035ff
Giorgio Basile <giorgio.basile4@gmail.com>
2016-11-18 01:20:56 +0100

    CoAP plugin simulate false

--- chapter7-implementation/part1-2-direct-gateway/wot-pi/wot-server.js
+++ chapter7-implementation/part1-2-direct-gateway/wot-pi/wot-server.js
@@ -11,7 +11,7 @@ var pirPlugin = require('./plugins/internal/pirPlugin'),
 pirPlugin.start({'simulate': true, 'frequency': 2000});
 ledsPlugin.start({'simulate': true, 'frequency': 10000});
 dhtPlugin.start({'simulate': true, 'frequency': 10000});
-coapPlugin.start({'simulate': true, 'frequency': 10000});
+coapPlugin.start({'simulate': false, 'frequency': 10000});
 
 var server = httpServer.listen(resources.pi.port, function(){
 	console.log('HTTP server started...');
